6e9459c771c343d21733a7d9b3ef80fb5f1b48d5
ido <hazanido@gmail.com>
2022-03-28 10:51:05 +0300

    lottery

--- Lottery.py
+++ Lottery.py
@@ -1,9 +1,9 @@
 import time
 
-options = ['integretion', 'shoresh', 'kiruvim', 'linear']
-input = int(input("Please enter ****:"))
+input = int(input('Enter id:'))
+
 random = (((time.localtime().tm_hour+time.localtime().tm_min)*time.localtime().tm_sec)//13) + input
-print(options[random % len(options)])
+print((random %12) +19)
 
 
 
